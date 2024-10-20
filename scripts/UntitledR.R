c52e2d843ca3c7f5e593ba94a347f75b15ffeb82
Elaine Yi <156034203+Elaineyi1@users.noreply.github.com>
2024-10-20 18:02:37 -0400

    Delete scripts/UntitledR.R

--- scripts/UntitledR.R
+++
@@ -1,8 +0,0 @@
-library(dplyr)
-library(tidyverse)
-library(here)
-library(janitor)
-library(knitr)
-
-poll_raw <- read.csv(here("downloads/election_prediction/inputs/data/president_polls.csv"))
-head(poll_raw)
